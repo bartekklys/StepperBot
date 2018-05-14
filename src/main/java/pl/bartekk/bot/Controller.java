99fe5d30d366a9676b23c7f2b2a651055b07907f
Bartosz Kłys <Bartosz_Klys@epam.com>
2018-05-14 10:53:27 +0200

    Rotate motor image

--- src/main/java/pl/bartekk/bot/Controller.java
+++ src/main/java/pl/bartekk/bot/Controller.java
@@ -1,13 +1,17 @@
 package pl.bartekk.bot;
 
+import javafx.animation.Animation;
+import javafx.animation.RotateTransition;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
+import javafx.scene.image.ImageView;
 import javafx.scene.paint.Paint;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class Controller {
 
@@ -15,6 +19,8 @@ public class Controller {
     public Button button;
     public ComboBox portID;
     public Label statusLabel;
+    public ImageView motor1;
+    public Button rotateButton;
 
     public void handleConnectButtonClick() {
         if (statusLabel.getText().equals(ConnectionStatus.DISCONNECTED)) {
@@ -43,5 +49,17 @@ public class Controller {
         alert.setHeaderText("StepperBot v1");
         alert.setContentText("2018 Bartosz Kłys, WIMiIP\n" + "Email: klys.bartosz@gmail.com");
         alert.show();
+    }
+
+    public void rotateImage() {
+
+        RotateTransition rotateTransition = new RotateTransition(Duration.seconds(10), motor1);
+        rotateTransition.setFromAngle(0);
+        rotateTransition.setToAngle(3600);
+        if (rotateTransition.getStatus() == Animation.Status.RUNNING) {
+            rotateTransition.pause();
+        } else {
+            rotateTransition.play();
+        }
     }
 }
