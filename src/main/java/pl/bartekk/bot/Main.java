3483f9e3cc9878a11f8924b29498c1251416ee18
Bartosz Kłys <Bartosz_Klys@epam.com>
2018-05-14 12:59:30 +0200

    New app icon and some other stuff

--- src/main/java/pl/bartekk/bot/Main.java
+++ src/main/java/pl/bartekk/bot/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,8 +12,9 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("stepper.fxml"));
-        primaryStage.setTitle("Stepper");
+        primaryStage.setTitle("StepperBot");
         primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.getIcons().add(new Image("pl/bartekk/bot/images/icon.png"));
         primaryStage.show();
     }
 
